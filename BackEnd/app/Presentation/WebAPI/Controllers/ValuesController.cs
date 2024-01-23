5efd9385a7b0b8ed62cc33f01257f77c7d341430
vubadien <badien2172004@gmail.com>
2024-01-24 01:52:21 +0700

    controller

---
+++ BackEnd/app/Presentation/WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,31 @@
+﻿using Application.Features.Users.command.CreateUser;
+using Application.Features.Users.queries.GetAll;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IMediator mediator;
+        public ValuesController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllUser()
+        {
+            var reponse = await mediator.Send(new GetAllUserQueryRequest());
+            return Ok(reponse);
+        }
+        [HttpPost] 
+        public async Task<IActionResult> CreateUser (CreateUsedCommandRequest requeste)
+        {
+            await mediator.Send(requeste);
+            return Ok();
+        }
+    }
+}
