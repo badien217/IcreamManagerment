55da3d44c6c8c518f382871e110e2457b2bc4723
vubadien <badien2172004@gmail.com>
2024-01-24 02:01:29 +0700

    convert get all

--- BackEnd/app/core/Application/Features/Users/command/CreateUser/CreateUsedCommandRequest.cs
+++ BackEnd/app/core/Application/Features/Users/command/CreateUser/CreateUsedCommandRequest.cs
@@ -1,18 +1,21 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Application.Features.Users.queries
+namespace Application.Features.Users.command.CreateUser
 {
-    public class GetAllUserQueryReponse
+    public class CreateUsedCommandRequest :IRequest
     {
         public string Username { get; set; }
+        public string Password { get; set; }
         public string Email { get; set; }
         public string phone { get; set; }
         public string SubcriptionType { get; set; }
         public bool PaymentStatus { get; set; }
-       
+        public int RoleId { get; set; }
+
     }
 }
--- BackEnd/app/core/Application/Features/Users/queries/GetAll/GetAllUserQueryRequest.cs
+++ BackEnd/app/core/Application/Features/Users/queries/GetAll/GetAllUserQueryRequest.cs
@@ -6,7 +6,7 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Application.Features.Users.queries
+namespace Application.Features.Users.queries.GetAll
 {
     public class GetAllUserQueryRequest : IRequest<IList<GetAllUserQueryReponse>>
     {
