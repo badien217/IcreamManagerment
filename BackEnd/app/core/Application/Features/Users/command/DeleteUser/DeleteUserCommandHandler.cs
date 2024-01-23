86e05a9330cf1c4d8ffcb1c476b3e7c7e906ead3
vubadien <badien2172004@gmail.com>
2024-01-24 03:51:40 +0700

    delete and update user

---
+++ BackEnd/app/core/Application/Features/Users/command/DeleteUser/DeleteUserCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using persistence.Interfaces.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Users.command.DeleteUser
+{
+    public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommandRequest>
+    {
+        public readonly IUnitOfWork unitOfWork;
+        public DeleteUserCommandHandler(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+        public async Task Handle(DeleteUserCommandRequest request, CancellationToken cancellationToken)
+        {
+            var users = await unitOfWork.GetReadReponsitory<User>().GetAsync(x => x.Id == request.Id && x.Status != "huy");
+            users.Status= "huy";
+            await unitOfWork.GetWriteReponsitory<User>().UpdateAsync(users);
+            await unitOfWork.SaveAsync();
+
+
+        }
+    }
+}
--- BackEnd/app/core/Application/Features/Users/command/UpdateUser/UpdateUserCommandReuquest.cs
+++ BackEnd/app/core/Application/Features/Users/command/UpdateUser/UpdateUserCommandReuquest.cs
@@ -5,10 +5,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Application.Features.Users.command.CreateUser
+namespace Application.Features.Users.command.UpdateUser
 {
-    public class CreateUsedCommandRequest :IRequest
+    public class UpdateUserCommandReuquest : IRequest
     {
+        public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
@@ -16,7 +17,6 @@ namespace Application.Features.Users.command.CreateUser
         public string SubcriptionType { get; set; }
         public bool PaymentStatus { get; set; }
         public int RoleId { get; set; }
-        public string Status { get; set; }
 
     }
 }
