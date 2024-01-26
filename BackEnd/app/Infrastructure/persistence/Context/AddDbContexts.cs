1a8b80ff1ad76d1a60cab9019825c2ae8fc8d852
kimmpoi <truykichlove1@gmail.com>
2024-01-26 13:59:25 +0700

    .

---
+++ BackEnd/app/Infrastructure/persistence/Context/AddDbContexts.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace persistence.Context
+{
+    public class AddDbContexts : DbContext
+    {
+        public AddDbContexts() { }
+        public AddDbContexts(DbContextOptions options) : base(options) {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
+        public DbSet<Flavor> Flavor { get; set; }
+        public DbSet<IceCream> IceCreams { get;set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<OrderDetail> OrderDetail { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Step> Steps { get; set; }
+       protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        
+
+
+    }
+}
