d2fde7bf989b257697527e729aa7a2044d1913b3
kimmpoi <truykichlove1@gmail.com>
2024-01-26 13:55:12 +0700

    merge

---
+++ BackEnd/app/core/Application/Features/Users/queries/GetAll/GetAllUserQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using persistence.Interfaces.UnitOfWorks;
+using Domain.Entities;
+using Application.Interfaces.AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Application.Dtos;
+
+namespace Application.Features.Users.queries.GetAll
+{
+    public class GetAllUserQueryHandler : IRequestHandler<GetAllUserQueryRequest, IList<GetAllUserQueryReponse>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IAutoMapper _mapper;
+        public GetAllUserQueryHandler(IUnitOfWork unitOfWork, IAutoMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<IList<GetAllUserQueryReponse>> Handle(GetAllUserQueryRequest request, CancellationToken cancellationToken)
+        {
+            var user = await _unitOfWork.GetReadReponsitory<User>().GetAllAsync(include: x => x.Include(b => b.role));
+            _mapper.Map<RoleDto>(new Role() );
+            
+            var map = _mapper.Map<GetAllUserQueryReponse, User>(user);
+            return map;
+        }
+    }
+}
---
+++ BackEnd/app/core/Application/Features/Users/queries/GetAll/GetAllUserQueryReponse.cs
@@ -0,0 +1,21 @@
+﻿using Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Users.queries.GetAll
+{
+    public class GetAllUserQueryReponse
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string phone { get; set; }
+        public string SubcriptionType { get; set; }
+        public bool PaymentStatus { get; set; }
+        public RoleDto roleId { get;set; }  
+        public string Status { get; set; }
+
+    }
+}
