f736a3c39abe775400effe47a0aeafb33a913c45
kimmpoi <truykichlove1@gmail.com>
2024-01-28 14:43:38 +0700

    commit

--- BackEnd/app/Infrastructure/persistence/Context/AddDbContexts.cs
+++ BackEnd/app/Infrastructure/persistence/Context/AddDbContexts.cs
@@ -14,7 +14,7 @@ namespace persistence.Context
         public AddDbContexts() { }
         public AddDbContexts(DbContextOptions options) : base(options) {
         }
-        public DbSet<User> Users { get; set; }
+        public DbSet<Book> Users { get; set; }
         public DbSet<Admin> Admins { get; set; }
         public DbSet<Book> Books { get; set; }
         public DbSet<Feedback> Feedbacks { get; set; }
