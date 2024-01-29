febba28783ecd3a68c9707be834779d59ed44c0c
vubadien <badien2172004@gmail.com>
2024-01-29 10:56:00 +0700

    ..

--- BackEnd/app/Presentation/WebAPI/Controllers/ValuesController.cs
+++ BackEnd/app/Presentation/WebAPI/Controllers/ValuesController.cs
@@ -2,6 +2,11 @@
 using Application.Features.Users.command.DeleteUser;
 using Application.Features.Users.command.UpdateUser;
 using Application.Features.Users.queries.GetAll;
+using Application.Features.Feedbacks.Queries.GetAll;
+using Application.Features.Feedbacks.Command.CreateFeedbacks;
+using Application.Features.Feedbacks.Command.UpdateFeedbacks;
+using Application.Features.Feedbacks.Command.DeleteFeedbacks;
+using Application.Features.Feedbacks.Queries.GetById;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -20,26 +25,32 @@ namespace WebAPI.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAllUser()
         {
-            var reponse = await mediator.Send(new GetAllUserQueryRequest());
+            var reponse = await mediator.Send(new GetAllFeedbackRequest());
             return Ok(reponse);
         }
         [HttpPost] 
-        public async Task<IActionResult> CreateUser (CreateUsedCommandRequest requeste)
+        public async Task<IActionResult> CreateUser (CreateFeedbackCommandRequest requeste)
         {
             await mediator.Send(requeste);
             return Ok();
         }
         [HttpPost]
-        public async Task<IActionResult> UpdateUser(UpdateUserCommandHandler requeste)
+        public async Task<IActionResult> UpdateUser(UpdateFeedbackCommandRRequest requeste)
         {
             await mediator.Send(requeste);
             return Ok();
         }
         [HttpPost]
-        public async Task<IActionResult> DeleteUser(DeleteUserCommandRequest requeste)
+        public async Task<IActionResult> DeleteUser(DeleteFeedbackCommandRequest requeste)
         {
             await mediator.Send(requeste);
             return Ok();
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllUserbyid()
+        {
+            var reponse = await mediator.Send(new GetFeebbackByIDQueriesRequest());
+            return Ok(reponse);
         }
     }
 }
