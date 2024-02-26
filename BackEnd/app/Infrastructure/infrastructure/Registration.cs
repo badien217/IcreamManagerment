5aab3d1fe0754d40f4f184957ca6cdea86a14412
vubadien <badien2172004@gmail.com>
2024-02-26 11:41:49 +0700

    token services

--- BackEnd/app/Infrastructure/infrastructure/Registration.cs
+++ BackEnd/app/Infrastructure/infrastructure/Registration.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Application.Interfaces.Token;
+using infrastructure.Token;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +12,41 @@ using System.Threading.Tasks;
 
 namespace infrastructure
 {
-    public class Registration
+    public static class Registration
     {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<TokenSetting>(configuration.GetSection("JWT"));
+            services.AddTransient<ITokenServices, TokenServices>();
+/*
+            services.Configure<RedisCacheSettings>(configuration.GetSection("RedisCacheSettings"));
+            services.AddTransient<IRedisCacheService, RedisCacheService>();*/
+
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, opt =>
+            {
+                opt.SaveToken = true;
+                opt.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"])),
+                    ValidateLifetime = false,
+                    ValidIssuer = configuration["JWT:Issuer"],
+                    ValidAudience = configuration["JWT:Audience"],
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+            /*
+            services.AddStackExchangeRedisCache(opt =>
+            {
+                opt.Configuration = configuration["RedisCacheSettings:ConnectionString"];
+                opt.InstanceName = configuration["RedisCacheSettings:InstanceName"];
+            });*/
+        }
     }
 }
--- BackEnd/app/Infrastructure/infrastructure/Token/TokenServices.cs
+++ BackEnd/app/Infrastructure/infrastructure/Token/TokenServices.cs
@@ -1,11 +1,14 @@
 ﻿using Application.Interfaces.Token;
 using Domain.Entities;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,19 +19,69 @@ namespace infrastructure.Token
     {
         private readonly UserManager<User> userManager;
         private readonly TokenSetting tokenSettings;
-        public Task<JwtSecurityToken> CreateToken(User user, IList<string> roles)
+        public TokenServices(IOptions<TokenSetting> options, UserManager<User> userManager)
         {
-            throw new NotImplementedException();
+            tokenSettings = options.Value;
+            this.userManager = userManager;
+        }
+        public async Task<JwtSecurityToken> CreateToken(User user, IList<string> roles)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email)
+            };
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenSettings.Secret));
+
+            var token = new JwtSecurityToken(
+                issuer: tokenSettings.Issuer,
+                audience: tokenSettings.Audience,
+                expires: DateTime.Now.AddMinutes(tokenSettings.TokenValidityInMunitues),
+                claims: claims,
+                signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
+                );
+
+            await userManager.AddClaimsAsync(user, claims);
+
+            return token;
         }
 
         public string GenerateRefreshToken()
         {
-            throw new NotImplementedException();
+            var randomNumber = new byte[64];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(randomNumber);
+            return Convert.ToBase64String(randomNumber);
         }
 
         public ClaimsPrincipal? GetPrincipalFromExpiredToken(string? token)
         {
-            throw new NotImplementedException();
+            TokenValidationParameters tokenValidationParamaters = new()
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenSettings.Secret)),
+                ValidateLifetime = false
+            };
+
+            JwtSecurityTokenHandler tokenHandler = new();
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParamaters, out SecurityToken securityToken);
+            if (securityToken is not JwtSecurityToken jwtSecurityToken
+                || !jwtSecurityToken.Header.Alg
+                .Equals(SecurityAlgorithms.HmacSha256,
+                StringComparison.InvariantCultureIgnoreCase))
+                throw new SecurityTokenException("token not found.");
+
+            return principal;
+
         }
     }
 }
