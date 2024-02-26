fb9257138accf4c4544023ac30928525ab5e3543
vubadien <badien2172004@gmail.com>
2024-02-26 11:40:07 +0700

    .

--- BackEnd/app/Presentation/WebAPI/Program.cs
+++ BackEnd/app/Presentation/WebAPI/Program.cs
@@ -3,7 +3,19 @@ using Microsoft.Data.SqlClient;
 using persistence;
 using Application;
 using Mapper;
+using Microsoft.OpenApi.Models;
+using infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder => builder
+            .WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials());
+});
 
 // Add services to the container.
 
@@ -11,12 +23,48 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 var env = builder.Environment;
 builder.Configuration.SetBasePath(env.ContentRootPath).AddJsonFile("appsettings.json", optional: false);
 builder.Services.AddPersistence(builder.Configuration);
+builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 builder.Services.AddMapper();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Project Ice Cream", Version = "v1", Description = "Ice Cream Api Client" });
+    c.AddSecurityDefinition("client", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "client",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "this is project asp net core api"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "client"
+                }
+            },
+            Array.Empty<string>()
+        }
+
+    });
+});
+
+
 var app = builder.Build();
+
+app.UseCors("AllowSpecificOrigin");
 
 
 // Configure the HTTP request pipeline.
